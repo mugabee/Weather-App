cd1b83caa329f7e472638fa14ba6f2f62ec8131b
mugabe richard <mugabe.richard.37@gmail.com>
2021-07-20 09:37:59 +0200

    Initial Commit

---
+++ MyWeather/MyWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyWeatherApp.swift
+//  MyWeather
+//
+//  Created by MUGABE RICHARD on 20/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct MyWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
