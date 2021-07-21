57d362899dbaa6fbf8ef26c35278ce1d0af6854b
mugabe richard <mugabe.richard.37@gmail.com>
2021-07-21 08:20:34 +0200

    creating the ViewModel

--- MyWeather/WeatherViewModel.swift
+++ MyWeather/WeatherViewModel.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 private let defaultIcon = "?"
-private let iconMap = {
+private let iconMap = [
     "Drizzle" : "🌧",
     "Thunderstorm" : "🌩",
     "Rain" : "🌨",
@@ -16,11 +16,27 @@ private let iconMap = {
     "Cleaar" : "☀️",
     "Clouds": "☁️",
     
-}
+]
 
 public class WeatherViewModel: ObservableObject {
     @Published var cityName: String = "City Name"
     @Published var temperature: String = "--"
     @Published var weatherDescription: String = "--"
     @Published var weatherIcon: String = defaultIcon
+    
+    public let weatherService: WeatherService
+    
+    public init(weatherService: WeatherService) {
+        self.weatherService = weatherService
+            
+        }
+    public func refresh() {
+        weatherService.loadWeatherData { Weather in
+            DispatchQueue.main.async {
+                self.cityName = Weather.city
+                self.temperature = "\(Weather.tempeature)°C"
+            }
+            
+        }
+    }
 }
